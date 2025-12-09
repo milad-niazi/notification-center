3bf0ab9d3cbb8ea0079b717e8a6d5faa96896e94
Milad Niazi <RealMiladNiazi@gmail.com>
2025-12-09 19:41:07 +0330

    Fix fillable properties in NotificationLog model

--- app/Models/NotificationLog.php
+++ app/Models/NotificationLog.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class NotificationLog extends Model
 {
-    //
+    use HasFactory;
+
+    // همه فیلدهایی که mass assignment می‌شوند را اضافه کن
+    protected $fillable = [
+        'channel',
+        'recipient',
+        'template_key',
+        'data',
+        'status',
+    ];
 }
