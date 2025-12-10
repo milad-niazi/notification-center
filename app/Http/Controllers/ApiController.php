a9bd9c27dba9b1d7b74f6d69daa399c0cddaff81
Milad Niazi <RealMiladNiazi@gmail.com>
2025-12-10 21:32:02 +0330

    Update ApiController to add general API response methods

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,8 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
+use App\Services\Notification\NotificationService;
 
 class ApiController extends Controller
 {
     use ApiResponser;
+    public function sendNotification(Request $request)
+    {
+        $service = new NotificationService();
+
+        $success = $service->send(
+            $request->channel,
+            $request->template,
+            $request->to,
+            $request->data
+        );
+
+        return response()->json([
+            "success" => $success
+        ]);
+    }
 }
