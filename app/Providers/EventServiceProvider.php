92534aa28629d4d5f51233554ef2ad403328468b
Milad Niazi <RealMiladNiazi@gmail.com>
2025-12-10 23:38:40 +0330

    Register Template events and listeners in EventServiceProvider - Map TemplateCreated, TemplateUpdated, TemplateDeleted to listeners

---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        \App\Events\TemplateCreated::class => [
+            \App\Listeners\Template\TemplateCreatedListener::class,
+        ],
+        \App\Events\TemplateUpdated::class => [
+            \App\Listeners\Template\TemplateUpdatedListener::class,
+        ],
+        \App\Events\TemplateDeleted::class => [
+            \App\Listeners\Template\TemplateDeletedListener::class,
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
