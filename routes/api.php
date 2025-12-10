6469e9c92ba8d6e02b23000184db7af0e36eedcd
Milad Niazi <RealMiladNiazi@gmail.com>
2025-12-10 21:32:02 +0330

    Add API routes for notification templates and sending notifications

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\Api\TemplateController;
 use App\Http\Controllers\Api\NotificationController;
 
 Route::post('/send-notification', [NotificationController::class, 'send']);
+Route::apiResource('templates', TemplateController::class);
